a31447db3c6a719fe0a01d696d8671ec493a4979
Sagar Admane <sagaradmane6@gmail.com>
2025-03-23 23:42:53 +0530

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,21 @@
-import React from 'react'
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import HomePage from './components/Home Page/HomePage'
-import SignUp from './components/Signup/SignUp'
-import Login from './components/Login/Login'
-import AccountSettings from './components/Account/Account'
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/Home Page/HomePage";
+import SignUp from "./components/Signup/SignUp";
+import Login from "./components/Login/Login";
+import AccountSettings from "./components/Account/Account";
 
 function App() {
-
-  const router = createBrowserRouter([
-    {
-      path : "/",
-      element : <HomePage />
-    },
-    {
-      path: "/signup",
-      element : <SignUp />
-    },
-    {
-      path : "/login",
-      element : <Login />
-    },
-    {
-      path : "/account",
-      element : <AccountSettings />
-    }
-  ])
-
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/account" element={<AccountSettings />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
